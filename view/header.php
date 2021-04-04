fb952b85c603fc92150dcc0a77fc06493091dcae
brendaRojas <breendr.17@gmail.com>
2021-04-03 19:32:21 -0600

    Se ha cargado el header y la navegación. Se han agregado las imagenes

--- view/header.php
+++ view/header.php
@@ -6,6 +6,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     
     <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Dancing+Script:wght@400;600;700&display=swap" rel="stylesheet"> 
+    <link href="https://fonts.googleapis.com/css2?family=Raleway:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet"> 
     <link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300;0,400;0,600;0,700;1,300&family=Roboto:ital,wght@0,100;0,400;0,700;0,900;1,100;1,400&display=swap" rel="stylesheet"> 
     <script src="https://kit.fontawesome.com/f0776b5046.js" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="css/style.css" class="stylesheet">
@@ -13,14 +15,24 @@
     <title>Aplicación E-Commerce</title>
 </head>
 <body>
-    <header class="contenedor">
-        <h1 class="logo izquierda">Logo</h1>
+    <header class="contenedor header no-padding">
+        <a href="index.php"><h1 class="logo no-margin izquierda">Coraly</h1></a>
 
-        <nav class="menu derecha">
-			<ul>
-    			<li><a href="#">Mi Cuenta<i class="fas fa-user"></i></a></li>
-				<li><a href="#">Mi Carrito<i class="fas fa-shopping-cart"></i></a></li>
-				<li><a href="#">Contacto<i class="icono fa fa-envelope"></i></a></li>
-			</ul>
-		</nav>
+        <nav class="navegacion derecha">
+            <a href="#" class="navegacion__enlace"><i class="icono fas fa-user"></i><p>mi cuenta</p></a>
+            <a href="#" class="navegacion__enlace"><i class="icono fas fa-shopping-cart"></i><p>mi carrito</p></a>
+            <a href="#" class="navegacion__enlace"><i class="icono fas fa-envelope"></i><p>Contacto</p></a>
+        </nav>
     </header>
+
+    <div class=" menu">
+        <ul class="contenedor">
+            <li class="menu__enlace"><a href="#"><p>Todas las categorias </p></a></li>
+            <li class="menu__enlace"><a href="#"><p>Blusas</p></i></a></li>
+            <li class="menu__enlace"><a href="#"><p>Playeras</p></i></a></li>
+            <li class="menu__enlace"><a href="#"><p>Pantalones</p></i></a></li>
+            <li class="menu__enlace"><a href="#"><p>Vestidos</p></i></a></li>
+            <li class="menu__enlace"><a href="#"><p>Zapatos</p></i></a></li>
+        </ul>
+    </div>
+
--- view/index.view.php
+++ view/index.view.php
@@ -2,8 +2,12 @@
 <?php require 'header.php'; ?>
 
 <!-- Index view -->
-
+    <section class="contenedor">
+        <ul class="slider">
+            <img class="slider__img" src="img/slider.jpg" alt="">
+        </ul>
+    </section>
 
 
 <!-- Footer -->
-<?php require 'footer.php'; ?>
+<?php //require 'footer.php'; ?>
