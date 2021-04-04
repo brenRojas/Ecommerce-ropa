89aa04966913bce6b8ead621a6c343098e66ba48
brendaRojas <breendr.17@gmail.com>
2021-04-03 23:13:46 -0600

    Se carga la versión1, index.view.php

--- view/footer.php
+++ view/footer.php
@@ -1,5 +1,20 @@
-<footer>
-	<p class="">Footer</p>
+<footer class="bgFooter">
+	<div class="contenedor footer">
+		<a href="index.php"><h1 class="logo__footer no-margin">Coraly</h1></a>
+		<div>
+			<p class="preguntas">Preguntas Frecuentes</p>
+			<div class="otros__red"><i class="icono fab fa-facebook"></i><p>Facebook</p></div>
+			<div class="otros__red"><i class="icono fab fa-instagram"></i><p>Instagram</p></div>
+			<div class="otros__red"><i class="icono fas fa-envelope"></i><p>coraly@correo.com</p></div>
+		</div>
+		<div class="otros">
+			<p class="politicas">Políticas de Compra</p>
+			<p>Envios</p>
+			<p>Medios de Pago</p>
+			<p>Cambios y Devoluciones</p>
+			<p>Aviso de Privacidad</p>
+		</div>
+	</>
 </footer>
 
 </body>
--- view/index.view.php
+++ view/index.view.php
@@ -6,13 +6,79 @@
     <!-- Slider -->
     <div class="slider">
         <ul>
-            <li><img src="img/slider1.jpg" alt=""></li>
-            <li><img src="img/slider2.jpg" alt=""></li>
-            <li><img src="img/slider3.jpg" alt=""></li>
-            <li><img src="img/slider4.jpg" alt=""></li>
+            <li><img src="img/slide1.jpg" alt=""></li>
+            <li><img src="img/slide2.jpg" alt=""></li>
+            <li><img src="img/slide3.jpg" alt=""></li>
+            <li><img src="img/slide4.jpg" alt=""></li>
         </ul>
     </div>
 
+    <!-- Lista de recomendaciones -->
+    <main class="contenedor sombra">
+        <h2>Productos Recomendados</h2>
+        <div class="grid">
+            <!-- Articulo -->
+            <div class="producto">
+                <a href="#">
+                    <img src="img/0.jpg" alt="imagen-producto" class="producto__img">
+                    <div class="producto__informacion">
+                        <p class="producto__nombre no-margin">NombreProducto</p>
+                        <p class="producto__precio no-margin">$100.00</p>
+                    </div>
+                </a>
+            </div>
+            <!-- Articulo -->
+            <div class="producto">
+                <a href="#">
+                    <img src="img/1.jpg" alt="imagen-producto" class="producto__img">
+                    <div class="producto__informacion">
+                        <p class="producto__nombre no-margin">NombreProducto</p>
+                        <p class="producto__precio no-margin">$100.00</p>
+                    </div>
+                </a>
+            </div>
+            <!-- Articulo -->
+            <div class="producto">
+                <a href="#">
+                    <img src="img/2.jpg" alt="imagen-producto" class="producto__img">
+                    <div class="producto__informacion">
+                        <p class="producto__nombre no-margin">NombreProducto</p>
+                        <p class="producto__precio no-margin">$100.00</p>
+                    </div>
+                </a>
+            </div>
+            <!-- Articulo -->
+            <div class="producto">
+                <a href="#">
+                    <img src="img/3.jpg" alt="imagen-producto" class="producto__img">
+                    <div class="producto__informacion">
+                        <p class="producto__nombre no-margin">NombreProducto</p>
+                        <p class="producto__precio no-margin">$100.00</p>
+                    </div>
+                </a>
+            </div>
+            <!-- Articulo -->
+            <div class="producto">
+                <a href="#">
+                    <img src="img/4.jpg" alt="imagen-producto" class="producto__img">
+                    <div class="producto__informacion">
+                        <p class="producto__nombre no-margin">NombreProducto</p>
+                        <p class="producto__precio no-margin">$100.00</p>
+                    </div>
+                </a>
+            </div>
+            <!-- Articulo -->
+            <div class="producto">
+                <a href="#">
+                    <img src="img/5.jpg" alt="imagen-producto" class="producto__img">
+                    <div class="producto__informacion">
+                        <p class="producto__nombre no-margin">NombreProducto</p>
+                        <p class="producto__precio no-margin">$100.00</p>
+                    </div>
+                </a>
+            </div>
+        </div>
 
+    </main>
 <!-- Footer -->
-<?php //require 'footer.php'; ?>
+<?php require 'footer.php'; ?>
