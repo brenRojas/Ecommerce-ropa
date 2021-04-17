c92b9c4af1753146c26302f39fb1aa90fe616724
Alfredo Cajica <alfredo_canez@hotmail.com>
2021-04-16 21:53:23 -0500

    [Nosotros] Se agrega texto complementario de la pantalla

--- nosotros.php
+++ nosotros.php
@@ -2,6 +2,14 @@
 
     <main class="contenedor sombra">
         <h1>Nosotros</h1>
+        <justify>Coraly es una empresa mexicana fundada en la ciudad de Puebla, dedicada a la venta 
+        de accesorios basados en plata y ropa para mujer, nuestra meta es ofrecer productos de calidad
+        para destacar las personalidades y potenciar actitudes de nuestros clientes, atendiendo
+        sus necesidades.</br>Somos entusiastas en brindar servicios y productos 
+        de alta calidad para cumplir las expectativas, brindando un servicio puntual, cumpliendo
+        en tiempo y forma con nuestros clientes para posicionarnos como la mejor opción
+        del mercado.</justify>
+         
     </main>
 
 <?php require 'view/footer.php'; ?>
