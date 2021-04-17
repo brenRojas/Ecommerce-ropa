86e4367161835607128219b5bce1c2b5aecf892c
Alfredo Cajica <alfredo_canez@hotmail.com>
2021-04-16 21:53:58 -0500

    [Mis direcciones] se agrega php y view para direcciones de usuario
    se modifica css para tabla

---
+++ mis_direcciones.php
@@ -0,0 +1,5 @@
+<?php
+
+require 'view/mis_direcciones.view.php';
+
+?>
---
+++ view/mis_direcciones.view.php
@@ -0,0 +1,24 @@
+<?php require 'header.php'; ?>
+
+    <main class="contenedor bg_cuenta sombra">
+        <div class="rectangulo">
+            <div class="cuenta">
+                <p class="titulo centrar-texto">Mi Cuenta</p>
+                <!-- Mis datos -->
+                <div class="form_group">
+                    <i class="icono fas fa-user" href="../mi_cuenta.php"></i>
+                </div>
+                
+                <!-- Direcciones -->
+                <div class="form_group">
+                    <i class="fas fa-address-book"></i>
+                    <div>
+                        Dirección:</br>
+                        Calle 31 ote 2560 72400, Col Anzurez, Puebla, Puebla
+                    </div>
+                </div>
+            </div>
+        </div>
+    </main>
+
+<?php require 'footer.php'; ?>
